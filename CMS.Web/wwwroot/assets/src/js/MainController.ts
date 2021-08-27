1c11954b9789f5b927df66d3717db740d825fc5f
VaclavKosak <vaclav.kosak@gmail.com>
2021-08-27 11:25:46 +0200

    Removed js from html file

--- CMS.Web/wwwroot/assets/src/js/MainController.ts
+++ CMS.Web/wwwroot/assets/src/js/MainController.ts
@@ -1,4 +1,37 @@
-class MainController{
-    constructor() {
+namespace web {
+    export class MainController{
+        constructor() {
+            this.init();
+        }
+
+        private init() {
+            this.tooltip();
+            this.validateForm();
+        }
+
+        private tooltip(){
+            let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+            tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+                // @ts-ignore
+                new bootstrap.Tooltip(tooltipTriggerEl)
+            });
+        }
+
+        private validateForm(){
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            let forms = document.querySelectorAll('.needs-validation')
+
+            // Loop over them and prevent submission
+            Array.prototype.slice.call(forms)
+                .forEach(function (form) {
+                    form.addEventListener('submit', function (event) {
+                        if (!form.checkValidity()) {
+                            event.preventDefault()
+                            event.stopPropagation()
+                        }
+                        form.classList.add('was-validated')
+                    }, false)
+                });
+        }
     }
 }
