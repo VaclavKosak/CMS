fa5902548c9e322a08a2424da5d1e3288628b48a
Václav Košák <vaclav.kosak@gmail.com>
2022-02-09 21:20:01 +0100

    Redirect log in user to admin page

--- CMS.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ CMS.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -83,7 +83,7 @@ namespace CMS.Web.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    return RedirectToAction("Index", "Home", new { area = "Admin"});
                 }
                 if (result.RequiresTwoFactor)
                 {
