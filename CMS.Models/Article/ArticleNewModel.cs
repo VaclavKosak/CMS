865ca7ed6d00912e138e94ac016b83fe194fdd97
Václav Košák <vaclav.kosak@gmail.com>
2022-09-28 21:06:17 +0200

    Article required parameters

--- CMS.Models/Article/ArticleNewModel.cs
+++ CMS.Models/Article/ArticleNewModel.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using CMS.Common.Enums;
 
 namespace CMS.Models.Article
 {
     public class ArticleNewModel
     {
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Url { get; set; }
         public DateTime PublicationDateTime { get; set; }
         public string Text { get; set; }
--- CMS.Models/Article/ArticleUpdateModel.cs
+++ CMS.Models/Article/ArticleUpdateModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using CMS.Common.Enums;
 using CMS.DAL.Entities;
 
@@ -8,8 +9,11 @@ namespace CMS.Models.Article
     public class ArticleUpdateModel
     {
         public Guid Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Url { get; set; }
         public DateTime PublicationDateTime { get; set; }
         public string Text { get; set; }
