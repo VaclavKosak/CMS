362d1518f85d5ac65f897a9495449915edf16bbd
Václav Košák <vaclav.kosak@gmail.com>
2023-02-09 11:02:23 +0100

    Google analytic

---
+++ CMS.Web/Models/AnalyticsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CMS.Web.Models;
+
+public class AnalyticsViewModel
+{
+    public List<string> AnalyticsCodes { get; set; }
+    public string ExtraScript { get; set; }
+}
